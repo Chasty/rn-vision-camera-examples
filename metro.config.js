45d6658b15fe104562fd70c4b2e3f84abf8887b3
Chasty <coderessence@gmail.com>
2024-01-23 16:16:12 -0500

    feat: first commit

---
+++ metro.config.js
@@ -0,0 +1,12 @@
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require("expo/metro-config");
+
+/** @type {import('expo/metro-config').MetroConfig} */
+const config = getDefaultConfig(__dirname);
+
+config.resolver.assetExts.push(
+  // Adds support for `.db` files for SQLite databases
+  "tflite"
+);
+
+module.exports = config;
