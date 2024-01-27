86589eca228e5bd1bd93446386ba8b19d2327087
Chasty <coderessence@gmail.com>
2024-01-26 20:10:39 -0500

    feat: use camera resize plugin v1.2.2

--- src/screens/MultipleDectionsAndPhoto.tsx
+++ src/screens/MultipleDectionsAndPhoto.tsx
@@ -225,9 +225,9 @@ export function MultipleDetectionsAndPhotoScreen() {
             width: selectedModel.input.shape.width,
             height: selectedModel.input.shape.height,
           },
-          //pixelFormat: "rgb",
-          //dataType: "uint8",
-          pixelFormat: "rgb-uint8",
+          pixelFormat: "rgb",
+          dataType: "uint8",
+          // pixelFormat: "rgb-uint8",
         });
         const rgbData = new Uint8Array(data);
 
--- src/screens/SingleDetectionReanimated.tsx
+++ src/screens/SingleDetectionReanimated.tsx
@@ -166,9 +166,9 @@ export function SingleDetectionReanimatedScreen() {
             width: selectedModel.input.shape.width,
             height: selectedModel.input.shape.height,
           },
-          //pixelFormat: "rgb",
-          //dataType: "uint8",
-          pixelFormat: "rgb-uint8",
+          pixelFormat: "rgb",
+          dataType: "uint8",
+          //pixelFormat: "rgb-uint8",
         });
         const rgbData = new Uint8Array(data);
 
